ef7ddddb56cf1dffdc543c4a633e586d4aaf7329
SOMEONE1703 <siphelel.1703@gmail.com>
2024-04-10 20:23:22 +0200

    hhayibo

--- api/message/index.js
+++ api/message/index.js
@@ -6,30 +6,10 @@ module.exports = async function (context, req) {
     // const responseMessage = name
     //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
     //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    cars.push(req.body);
     context.res.json( {
         // status: 200, /* Defaults to 200 */
         body: "responseMessage",
         text:cars
     });
-}
-module.exports.doSomething = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    if (req.method=="POST"){
-        cars.push(context.body);
-        context.res.json( {
-            // status: 200, /* Defaults to 200 */
-            body: "responseMessage",
-            text:cars
-        });
-    }
-    else{
-        context.res.json( {
-            // status: 200, /* Defaults to 200 */
-            // body: responseMessage
-            text:"Goodbye from Something else"
-        });
-    }
-
-    
 }
