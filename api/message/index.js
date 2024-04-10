5e1d43c9c778f85306bfe998d4cd1634a8990e49
SOMEONE1703 <siphelel.1703@gmail.com>
2024-04-10 21:01:13 +0200

    nakhuya

--- api/message/index.js
+++ api/message/index.js
@@ -1,4 +1,5 @@
 const cars=require('./cars.json');
+let next_ind=3;
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
@@ -6,10 +7,22 @@ module.exports = async function (context, req) {
     // const responseMessage = name
     //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
     //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-    cars.push(req.body);
-    context.res.json( {
-        // status: 200, /* Defaults to 200 */
-        body: "responseMessage",
-        text:cars
-    });
+    
+    if (req.body.key==="get"){
+        context.res.json( {
+            // status: 200, /* Defaults to 200 */
+            body: "responseMessage",
+            text:cars
+        });
+    }
+    else if(req.body.key==="post"){
+        cars.push(req.body.one);
+        context.res.json( {
+            // status: 200, /* Defaults to 200 */
+            body: "all good",
+        });
+    }
+    else if(req.boy.key==="adjust"){
+
+    }
 }
--- src/script.js
+++ src/script.js
@@ -4,37 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const carList = document.getElementById('carList');
     cars = [];
     loadCarsBtn.addEventListener('click', () => {
-        // fetch('/api/message')
-        //     .then(response => response.json())
-        //     .then(data => {
-        //         console.log("received");
-        //         console.log(data);
-        //         cars = data.text;
-        //         carList.innerHTML = '';
-        //         data.text.forEach((car, index) => {
-        //             const carCard = document.createElement('div');
-        //             carCard.classList.add('car-card');
-        //             carCard.innerHTML = `
-        //                 <h2>${car.make} ${car.model}</h2>
-        //                 <p><strong>Year:</strong> ${car.year}</p>
-        //                 <p><strong>Make:</strong> ${car.make}</p>
-        //                 <p><strong>Model:</strong> ${car.model}</p>
-        //                 <p><strong>Price:</strong> R${car.price}</p>
-        //                 <button class="btn btn-remove" data-index="${index}">Remove</button>
-        //             `;
-        //             carList.appendChild(carCard);
-        //         });
-        //     })
-        //     .catch(error => {
-        //         console.error('Error fetching car data:', error);
-        //     });
-        //##########################
         fetch('/api/message', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({make:"something",model:"else",year:2004,price:30000})
+            body: JSON.stringify({key:"get"})
         })
             .then(response => response.json())
             .then(data => {
@@ -67,12 +42,12 @@ document.addEventListener('DOMContentLoaded', () => {
     
 });
 function addCar(newCar) {
-    fetch('/api/doSomething', {
+    fetch('/api/message', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(newCar)
+        body: JSON.stringify({one:newCar,key:"add"})
     })
         .then(response => response.json())
         .then(data => {
